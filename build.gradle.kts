4b010acf8bacdec66ac3ed5c23b64639e3f5f1d4
BaseToucher <daffiestemu858@gmail.com>
2026-08-12 05:16:36 -0400

    Fix native launcher version reporting

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.gambleclient"
-version = "0.1.102"
+version = "0.1.103"
 
 val javafxVersion = "22.0.2"
 val javafxModuleNames = listOf("base", "graphics", "controls", "media", "web")
